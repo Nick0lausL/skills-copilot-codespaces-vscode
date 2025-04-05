223446ce4350e87237cfc09d0172c4a71f259c6d
Nikolai Derko <n.derko@lancaster.ac.uk>
2025-04-05 13:01:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <div className="skills-member__name">John Doe</div>
+      <div className="skills-member__skills">
+        <ul>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Node.js</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
